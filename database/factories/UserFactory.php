89a18c463275f19ebb17927508dbdaa45f7c6561
abod <abodi.alwosab12@gmail.com>
2024-08-12 23:04:39 +0300

    change test unit to test pest and make  DEBUGBAR_ENABLED =false in example.env

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserGenderEnum;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -19,9 +20,13 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'last_name' => $this->faker->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'phone' => $this->faker->regexify('7[0-9]{8}'), // Assuming phone starts with 7 and is 9 digits long
+            'gender' => $this->faker->randomElement(UserGenderEnum::values()), // Assuming gender enum has values method
+            'status' => $this->faker->boolean(), // Assuming status is a boolean
             'remember_token' => Str::random(10),
         ];
     }
@@ -33,7 +38,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,128 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    protected $authUser;
+
+    protected  function setUp(): void
+    {
+        parent::setUp();
+
+        // كتابة الصلاحيات (العمليات)
+        $permissions = [
+            'view_address',
+            'view_any_address',
+            'create_address',
+            'update_address',
+            'restore_address',
+            'restore_any_address',
+            'replicate_address',
+            'reorder_address',
+            'delete_address',
+            'delete_any_address',
+            'force_delete_address',
+            'force_delete_any_address',
+            'view_category',
+            'view_any_category',
+            'create_category',
+            'update_category',
+            'restore_category',
+            'restore_any_category',
+            'replicate_category',
+            'reorder_category',
+            'delete_category',
+            'delete_any_category',
+            'force_delete_category',
+            'force_delete_any_category',
+            'view_delivery::detail',
+            'view_any_delivery::detail',
+            'create_delivery::detail',
+            'update_delivery::detail',
+            'restore_delivery::detail',
+            'restore_any_delivery::detail',
+            'replicate_delivery::detail',
+            'reorder_delivery::detail',
+            'delete_delivery::detail',
+            'delete_any_delivery::detail',
+            'force_delete_delivery::detail',
+            'force_delete_any_delivery::detail',
+            'view_farm',
+            'view_any_farm',
+            'create_farm',
+            'update_farm',
+            'restore_farm',
+            'restore_any_farm',
+            'replicate_farm',
+            'reorder_farm',
+            'delete_farm',
+            'delete_any_farm',
+            'force_delete_farm',
+            'force_delete_any_farm',
+            'view_order',
+            'view_any_order',
+            'create_order',
+            'update_order',
+            'restore_order',
+            'restore_any_order',
+            'replicate_order',
+            'reorder_order',
+            'delete_order',
+            'delete_any_order',
+            'force_delete_order',
+            'force_delete_any_order',
+            'view_product',
+            'view_any_product',
+            'create_product',
+            'update_product',
+            'restore_product',
+            'restore_any_product',
+            'replicate_product',
+            'reorder_product',
+            'delete_product',
+            'delete_any_product',
+            'force_delete_product',
+            'force_delete_any_product',
+        ];
+        /*
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        } */
+
+        // (role)كتابة الصلاحيات
+        /*   $roles = [
+            'super_admin',
+            'farmer',
+            'supplier',
+            'delivery',
+            'content_manager',
+            'inspector'
+        ];
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        } */
+
+        // إنشاء دور وأذونات
+        $role = Role::first(); // استخدام أول دور تم إنشاؤه
+        $permission = Permission::pluck('id', 'id')->all();
+        $role->syncPermissions($permission);
+
+
+
+        // Create a user for authentication
+        $this->authUser = User::factory()->create();
+
+        $this->authUser->assignRole($role);
+
+        // Log in the user before each test
+        $this->actingAs($this->authUser);
+    }
 }
